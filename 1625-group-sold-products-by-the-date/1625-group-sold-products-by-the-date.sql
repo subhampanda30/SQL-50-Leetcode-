07e39ba4673c49bb8db29e89a308750b61629de3
SUBHAM KUMAR PANDA <95236327+subhampanda30@users.noreply.github.com>
2024-10-16 22:34:34 +0530

    Time: 1039 ms (29.12%), Space: 0B (100%) - LeetHub

---
+++ 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
@@ -0,0 +1,5 @@
+/* Write your T-SQL query statement below */
+select sell_date, count(product) num_sold, STRING_AGG(product,',') WITHIN GROUP (order by product) as products 
+from (select distinct * from Activities) as a 
+group by sell_date
+order by sell_date
